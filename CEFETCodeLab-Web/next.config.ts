c2462b2b8d7042eadba65c5deecedc8593e7e39a
devgui <guihermepereiradv@gmail.com>
2025-03-29 17:54:01 -0300

    feat(Web/next.config.ts): add NEXT_PUBLIC_API_URL for local API configuration

--- CEFETCodeLab-Web/next.config.ts
+++ CEFETCodeLab-Web/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: 'http://localhost:3010/v1',
+  }
 };
 
 export default nextConfig;
