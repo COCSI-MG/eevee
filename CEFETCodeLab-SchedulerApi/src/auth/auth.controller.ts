3481d85656495129dcafdcd0b2ac016046f66ff4
devgui <guihermepereiradv@gmail.com>
2025-03-29 17:55:06 -0300

    feat(AuthController): add user registration endpoint with error handling

--- CEFETCodeLab-SchedulerApi/src/auth/auth.controller.ts
+++ CEFETCodeLab-SchedulerApi/src/auth/auth.controller.ts
@@ -2,13 +2,15 @@ import {
   Controller,
   Post,
   Body,
-  UseGuards,
   UnauthorizedException,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginRequestDto } from './dto/request/login-request.dto';
-import { ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { ApiInternalServerErrorResponse, ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { LoginResponseDto } from './dto/response/login-response.dto';
+import { RegisterRequestDto } from './dto/request/register-request.dto';
+import { RegisterResponseDto } from './dto/response/register-response.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -17,13 +19,26 @@ export class AuthController {
   @Post('login')
   @ApiOkResponse({ type: LoginResponseDto })
   @ApiUnauthorizedResponse()
-  async create(@Body() createAuthDto: LoginRequestDto) {
-    const result = await this.authService.validateUserAndLogin(createAuthDto);
+  async create(@Body() loginAuthDto: LoginRequestDto) {
+    const result = await this.authService.validateUserAndLogin(loginAuthDto);
 
     if (result) {
       return result;
     }
 
     throw new UnauthorizedException();
+  }
+
+  @Post('register')
+  @ApiOkResponse({ type: RegisterResponseDto })
+  @ApiInternalServerErrorResponse()
+  async register(@Body() registerAuthDto: RegisterRequestDto) {
+    const result = await this.authService.registerUser(registerAuthDto);
+    if (result) {
+      return result;
+    }
+    throw new InternalServerErrorException(
+      'An error occurred while trying to register the user. Please try again later.',
+    );
   }
 }
