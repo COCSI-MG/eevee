39b22b016600be6c3cee3057d9abe985bff12f12
Ana-beatriz2 <caldas.ana9@gmail.com>
2025-04-12 21:58:36 -0300

    feat: cria rota para retornar as classes de um usuário

--- CEFETCodeLab-SchedulerApi/src/class/class.module.ts
+++ CEFETCodeLab-SchedulerApi/src/class/class.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ClassService } from './class.service';
 import { ClassController } from './class.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Class } from './entities/class.entity';
 import { UserClassModule } from 'src/user-class/user-class.module';
+import { RequestContextModule } from 'src/request-context/request-context.module';
+import { ClassService } from './class.service';
 
 @Module({
   controllers: [ClassController],
   providers: [ClassService],
-  imports: [TypeOrmModule.forFeature([Class]), UserClassModule],
+  imports: [TypeOrmModule.forFeature([Class]), UserClassModule, RequestContextModule],
+  exports: [ClassService],
 })
 export class ClassModule {}
--- CEFETCodeLab-SchedulerApi/src/class/class.service.ts
+++ CEFETCodeLab-SchedulerApi/src/class/class.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UseGuards } from '@nestjs/common';
+import { ForbiddenException, Injectable, UseGuards } from '@nestjs/common';
 import { CreateOrReplaceClassDto } from './dto/request/create-or-replace-class.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Class } from './entities/class.entity';
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 import { ClassHelper } from './class.helper';
 import { UserClassService } from 'src/user-class/user-class.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RequestContextService } from 'src/request-context/request-context.service';
 
 @Injectable()
 @UseGuards(JwtAuthGuard)
@@ -14,6 +15,7 @@ export class ClassService {
     @InjectRepository(Class)
     private readonly classRepository: Repository<Class>,
     private readonly userClassService: UserClassService,
+     private readonly requestContextService: RequestContextService
   ) {}
   async createOrReplace(createClassDto: CreateOrReplaceClassDto) {
     if (createClassDto.id) {
@@ -53,6 +55,25 @@ export class ClassService {
         relations: ['userClasses', 'userClasses.user', 'userClasses.class'],
       })
     ).map(ClassHelper.toResponseDto);
+  }
+
+  async findAllByUser(userId: number) {
+    const user = this.requestContextService.getUser();
+
+    if (user.userId !== userId && !user.isAdmin) {
+      throw new ForbiddenException('You are not authorized to access classes of other users.');
+    }
+
+    return (
+      await this.classRepository.find({
+        relations: ['userClasses', 'userClasses.user', 'userClasses.class'],
+        where: {
+          userClasses: {
+            user: { id: userId },
+          },
+        },
+      })
+    )
   }
 
   findOne(id: number) {
