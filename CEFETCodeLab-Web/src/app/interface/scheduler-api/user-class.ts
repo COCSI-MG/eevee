b8a509e00026a75cc42a1ae63dc4afe7217adcd3
devgui <guihermepereiradv@gmail.com>
2025-05-18 17:42:22 -0300

    feat: implement ClassesService and UsersService with CRUD operations for classes and users

--- CEFETCodeLab-Web/src/app/interface/scheduler-api/user-class.ts
+++ CEFETCodeLab-Web/src/app/interface/scheduler-api/user-class.ts
@@ -1,5 +1,10 @@
+import { Class } from "./class";
+import { User } from "./user";
+
 export interface UserClass {
   id: number;
   userId: number;
   classId: number;
+  user: User;
+  class: Class
 }
