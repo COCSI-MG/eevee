6efd1ae5cfe02a798e702440fb381bbe419f4322
devgui <guihermepereiradv@gmail.com>
2025-05-29 01:03:36 -0300

    feat: add dialog for viewing template content and update actions in template table

--- CEFETCodeLab-Web/src/app/admin/templates/page.tsx
+++ CEFETCodeLab-Web/src/app/admin/templates/page.tsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { TemplatesService } from '@/app/integration/scheduler-api/templates';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import { Edit, Eye, MoreHorizontal, Plus, Trash2 } from 'lucide-react';
+import { Code, Edit, Eye, MoreHorizontal, Plus, Trash2 } from 'lucide-react';
 import {
   Table,
   TableBody,
@@ -14,13 +14,19 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-} from '@radix-ui/react-dropdown-menu';
-import {
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenu,
+  DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { Route } from '@/app/routes';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 export default function TemplatePage() {
   const {
@@ -49,15 +55,20 @@ export default function TemplatePage() {
       <div className="border rounded-md">
         <Table>
           <TableHeader>
-            <TableHead className="cursor-pointer">
-              <div className="flex items-center">Title</div>
-            </TableHead>
-            <TableHead className="cursor-pointer">
-              <div className="flex items-center">Description</div>
-            </TableHead>
-            <TableHead className="cursor-pointer">
-              <div className="flex items-center">Actions</div>
-            </TableHead>
+            <TableRow>
+              <TableHead className="cursor-pointer">
+                <div className="flex items-center">Title</div>
+              </TableHead>
+              <TableHead className="cursor-pointer">
+                <div className="flex items-center">Description</div>
+              </TableHead>
+              <TableHead className="cursor-pointer">
+                <div className="flex items-center">Conteúdo</div>
+              </TableHead>
+              <TableHead className="cursor-pointer">
+                <div className="flex items-center">Actions</div>
+              </TableHead>
+            </TableRow>
           </TableHeader>
           <TableBody>
             {!isPending && isSuccess && (templates ?? 0).length === 0 && (
@@ -76,6 +87,35 @@ export default function TemplatePage() {
                   </TableCell>
                   <TableCell>{template.description}</TableCell>
                   <TableCell>
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <Button
+                          variant={'outline'}
+                          size="sm"
+                          className="text-slate-40 hover:text-white"
+                        >
+                          <Eye className="h-4 w-4" />
+                          Visualizar
+                        </Button>
+                      </DialogTrigger>
+                      <DialogContent className="bg-slate-800 border-slate-700 max-w-4xl max-h-[80vh]">
+                        <DialogHeader>
+                          <DialogTitle className="text-white flex items-center gap-2">
+                            <Code className="w-5 h-5" />
+                            {template.title}
+                          </DialogTitle>
+                        </DialogHeader>
+                        <div className="space-y-4">
+                          <div className="bg-slate-900 border borde-slate-600 rounded-md p4 max-h-[50vh] overflow-y-auto">
+                            <pre className="whitespace-pre-wrap break-words text-green-400">
+                              {template.templateContent}
+                            </pre>
+                          </div>
+                        </div>
+                      </DialogContent>
+                    </Dialog>
+                  </TableCell>
+                  <TableCell>
                     <DropdownMenu>
                       <DropdownMenuTrigger asChild>
                         <Button
@@ -87,20 +127,16 @@ export default function TemplatePage() {
                         </Button>
                       </DropdownMenuTrigger>
                       <DropdownMenuContent className="bg-slate-700 border-slate-600">
-                        <DropdownMenuItem className="text-slate-300 hover:text-white hover:bg-slate-600">
-                          <Eye className="w-4 h-4 mr-2" />
-                          Visualizar
-                        </DropdownMenuItem>
-                        <Link href={`/admin/templates/${template.id}`}>
+                        <Link href={`${Route.AdminTemplate}/${template.id}`}>
                           <DropdownMenuItem className="text-slate-300 hover:text-white hover:bg-slate-600">
                             <Edit className="w-4 h-4 mr-2" />
                             Editar
                           </DropdownMenuItem>
                         </Link>
-                        <DropdownMenuItem className="text-red-400 hover:text-red-300 hover:bg-slate-600">
+                        {/* <DropdownMenuItem className="text-red-400 hover:text-red-300 hover:bg-slate-600">
                           <Trash2 className="w-4 h-4 mr-2" />
                           Deletar
-                        </DropdownMenuItem>
+                        </DropdownMenuItem> */}
                       </DropdownMenuContent>
                     </DropdownMenu>
                   </TableCell>
