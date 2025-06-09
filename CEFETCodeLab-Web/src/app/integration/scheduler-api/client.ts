e0d0bbbd267428b113a9dbec200c6c81b06f6efb
devgui <guihermepereiradv@gmail.com>
2025-06-08 22:56:50 -0300

    refactor: remove commented-out response interceptor from axiosClientWithAuth

--- CEFETCodeLab-Web/src/app/integration/scheduler-api/client.ts
+++ CEFETCodeLab-Web/src/app/integration/scheduler-api/client.ts
@@ -35,23 +35,3 @@ axiosClientWithAuth.interceptors.request.use(
     return Promise.reject(error);
   },
 );
-
-// axiosClientWithAuth.interceptors.response.use(
-//   (response) => {
-//     // Any status code that lie within the range of 2xx cause this function to trigger
-//     return response;
-//   },
-//   (error) => {
-//     // Any status codes that falls outside the range of 2xx cause this function to trigger
-//     if (error.response.status === 401) {
-//       console.log("Unauthorized error");
-
-//       // Handle unauthorized error, e.g., redirect to login page
-//       AuthContext.clear();
-//       window.location.href = `/${Route.Login}`;
-//     }
-//     // You can add more error handling logic here
-
-//     return Promise.reject(error);
-//   }
-// );
