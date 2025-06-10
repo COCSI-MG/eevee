3f3e89be1978977651ee156d219b01bc3baf1ade
devgui <guihermepereiradv@gmail.com>
2025-06-09 23:28:15 -0300

    fix: update link for adding new assignment to use Route constant

--- CEFETCodeLab-Web/src/app/admin/assignments/page.tsx
+++ CEFETCodeLab-Web/src/app/admin/assignments/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { Plus } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import AssignmentsTable from '@/components/assignments-table';
+import { Route } from '@/app/routes';
 
 export default function AssignmentsAdminPage() {
   return (
@@ -9,7 +10,7 @@ export default function AssignmentsAdminPage() {
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold tracking-tight">Assignments</h1>
 
-        <Link href="/admin/assignments/new">
+        <Link href={`/${Route.AdminAssignmentCreate}`}>
           <Button variant={'outline'}>
             <Plus className="h-4 w-4 mr-2" />
             Add Assignment
