a7597873297aa5af706a913a716126297970e8d1
devgui <guihermepereiradv@gmail.com>
2025-06-09 23:40:12 -0300

    refactor: standardize string quotes and improve error handling in axios interceptors

--- CEFETCodeLab-Web/src/app/integration/scheduler-api/client.ts
+++ CEFETCodeLab-Web/src/app/integration/scheduler-api/client.ts
@@ -1,37 +1,48 @@
-"use client";
+'use client';
 
-import { AuthContext } from "@/app/context/auth-context";
-import { Route } from "@/app/routes";
+import { AuthContext } from '@/app/context/auth-context';
+import { Route } from '@/app/routes';
 // import { Route } from "@/app/routes";
-import axios from "axios";
+import axios from 'axios';
 
 export const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
 export const axiosClientWithAuth = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
 axiosClientWithAuth.interceptors.request.use(
   (config) => {
-    const accessToken = localStorage.getItem("access_token");
+    const accessToken = localStorage.getItem('access_token');
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
   },
   (error) => {
+    console.error('Request error:', error);
+    return Promise.reject(error);
+  }
+);
+
+axiosClientWithAuth.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
     if (error.response.status === 401) {
       AuthContext.clear();
       window.location.href = `/${Route.Login}`;
     }
+    console.error('Response error:', error);
     return Promise.reject(error);
-  },
+  }
 );
