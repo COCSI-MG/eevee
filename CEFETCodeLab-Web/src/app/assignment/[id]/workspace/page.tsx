bfd2c3d7a88a7757e86bbf93216d0156c7efc1dd
devgui <guilhermepereiradv@gmail.com>
2025-07-14 21:57:10 -0300

    feat(workspace): refactor assignment workspace component and update file handling logic

--- CEFETCodeLab-Web/src/app/assignment/[id]/workspace/page.tsx
+++ CEFETCodeLab-Web/src/app/assignment/[id]/workspace/page.tsx
@@ -1,322 +1,203 @@
 "use client";
 
-import type React from "react";
-
-import { useEffect } from "react";
-import { useMutation } from "@tanstack/react-query";
-import { useParams } from "next/navigation";
-import { SchedulingService } from "@/app/integration/scheduler-api/scheduling";
-import { toast } from "@/hooks/use-toast";
-import WorkspaceHeader from "@/components/workspace/header";
-import WorkspaceExplorer from "@/components/workspace/explorer";
-import WorkspaceEditor from "@/components/workspace/editor";
-import WorkspaceConsole from "@/components/workspace/console";
-import { FileType } from "@/types/shared";
-import {
-  upsertFileInStash,
-} from "@/app/integration/filestash";
-import { getFilePath } from "@/lib/file-path-utils";
-import { useAuthUser } from "@/hooks/use-auth-user";
+import { DEFAULT_ASSIGNMENT_TEMPLATE } from "@/app/admin/assignments/constants";
 import FileSaverService from "@/app/integration/scheduler-api/file-saver";
-import { usePreventUserActions } from "@/hooks/use-prevent-user-actions";
+import { SchedulingService } from "@/app/integration/scheduler-api/scheduling";
+import WorkspaceHeader from "@/components/workspace/header";
+import WorkspaceCodeEditor from "@/components/workspace/workspace-code-editor";
+import WorkspaceExplorer from "@/components/workspace/workspace-explorer";
 import { useFetchAssignment } from "@/hooks/use-assignments";
-import { useWindowFocus } from "@/hooks/use-window-focus";
-import { useFileStash } from "@/hooks/use-filestash";
-import { useWorskpaceResizing } from "@/hooks/use-workspace-resizing";
-import WindowFocusDialog from "@/components/window-focus-dialog";
-import { useWorkspace } from "@/hooks/use-workspace";
+import {
+  useFetchFromStash,
+  useFileStash,
+  useSaveInFileStash,
+} from "@/hooks/use-filestash";
+import { usePreventUserActions } from "@/hooks/use-prevent-user-actions";
+import { toast } from "@/hooks/use-toast";
+import { FileTreeData } from "@/types/shared";
+import { useMutation } from "@tanstack/react-query";
+import { FileStrucutre } from "filestash";
+import { useParams } from "next/navigation";
+import React from "react";
 
-export default function AssignmentWorkspace() {
+const defaultTreeData: FileTreeData[] = [
+  {
+    id: "1",
+    label: "src",
+    isSelectable: false,
+    isFile: false,
+    children: [
+      {
+        id: "2",
+        label: "index.js",
+        isSelectable: true,
+        isFile: true,
+      },
+    ],
+  },
+];
+
+export default function Page() {
   const { id } = useParams();
+  const [activeFile, setActiveFile] = React.useState<{
+    id: string;
+    name: string;
+  }>({
+    id: "2",
+    name: "index.js",
+  });
+  const [treeData, setTreeData] = React.useState<FileTreeData[]>([]);
+  const [defaultEditorValue, setDefaultEditorValue] = React.useState<string>(
+    DEFAULT_ASSIGNMENT_TEMPLATE
+  );
 
-  const { explorerWidth, consoleHeight, startResize } = useWorskpaceResizing();
-  const {
-    fileStructure,
-    setFileStructure,
-    newItem,
-    setNewItem,
-    setConsoleOutput,
-    activeFile,
-    setActiveFile,
-    activeFileContent,
-    setActiveFileContent,
-    activeLocalFilePath,
-    newItemRef,
-    getFileStruct,
-    getFileContentFromStash
-  } = useWorkspace();
-
-  const { focusCount, resetFocusCount } = useWindowFocus();
-
-  const { user } = useAuthUser();
-  const { data: assignment, isLoading } = useFetchAssignment(Number(id));
-
-  usePreventUserActions();
   useFileStash();
+  usePreventUserActions();
+
+  const { mutate: saveFileInStash } = useSaveInFileStash();
 
   const {
-    mutate: submitAssignment,
-    isPending,
-    data: workerResult,
-  } = useMutation({
+    mutate: fetchFileContentFromStash,
+    mutateAsync: fetchFileContentFromStashAsync,
+    data: fileContent,
+  } = useFetchFromStash();
+
+  const { data: assignmentData, isLoading: isAssignmentLoading } =
+    useFetchAssignment(Number(id));
+
+  const { mutate: submitAssignment } = useMutation({
     mutationKey: ["submit-assignment"],
-    mutationFn: () => {
+    mutationFn: async () => {
+      const fileKey = `assignment-${assignmentData?.id}-file-${activeFile.id}`;
+      const fileContent = await fetchFileContentFromStashAsync(fileKey);
+
       return SchedulingService.createScheduling({
         assignmentId: Number(id),
-        applicationFileContent: activeFileContent,
+        applicationFileContent: fileContent.toString(),
       });
     },
     onError: (error) => {
-      console.error("Error submitting assignment:", error);
-      setConsoleOutput((prev) => [
-        ...prev,
-        "Error: Ocorreu um erro ao submeter a tarefa.",
-      ]);
-    },
-  });
-
-  const {
-    mutate: saveFileInStash,
-    isPending: isSaving,
-    isError: isSavingError,
-    data: savedFile,
-  } = useMutation({
-    mutationKey: ["save-file"],
-    mutationFn: (activeFileStructure: FileType) => {
-      if (!assignment) {
-        console.error("No assignment data available");
-        return Promise.reject("No assignment data available");
-      }
-
-      const filePath = getFilePath(activeFileStructure, fileStructure);
-      const key = `${assignment.id}/${activeFileStructure.id}/${filePath}`;
-      console.log("activeFileContent:", activeFileContent);
-      return upsertFileInStash(
-        {
-          name: filePath,
-          data: activeFileContent,
-          size: activeFileContent.length,
-          createdAt: new Date().toISOString(),
-          updateAt: new Date().toISOString(),
-        },
-        key
-      );
-    },
-  });
-
-  const { mutate: saveFileAtServer, isPending: isSavingAtServer } = useMutation(
-    {
-      mutationKey: ["save-file-at-server"],
-      mutationFn: (file: File) => {
-        if (!assignment) {
-          console.error("No assignment data available");
-          return Promise.reject("No assignment data available");
-        }
-        if (!user) {
-          console.error("No user data available");
-          return Promise.reject("No user data available");
-        }
-        return FileSaverService.uploadFileToServer(
-          file,
-          Number(assignment.id),
-          Number(user.id)
-        );
-      },
-    }
-  );
-
-  useEffect(() => {
-    if (!isPending && workerResult) {
-      console.log("Worker result:", workerResult);
-      setConsoleOutput((prev) => [
-        ...prev,
-        `> Score: ${workerResult.score}`,
-        `> Passes: ${workerResult.passes}`,
-        `> Fails: ${workerResult.fails}`,
-        `Report: ${workerResult.report.replace(/\\n/g, "\n")}`,
-      ]);
-    }
-  }, [isPending, setConsoleOutput, workerResult]);
-
-  useEffect(() => {
-    if (newItem.isCreating && newItemRef.current) {
-      setTimeout(() => {
-        newItemRef.current?.focus();
-      }, 0);
-    }
-  }, [newItem.isCreating, newItemRef]);
-
-  useEffect(() => {
-    const localStorageFileStructure = localStorage.getItem(
-      `file-Structure-assignment-${id}`
-    );
-    if (localStorageFileStructure) {
-      const parsedFileStructure = JSON.parse(localStorageFileStructure);
-      setFileStructure(parsedFileStructure);
-    }
-  }, [id, setFileStructure]);
-
-  useEffect(() => {
-    localStorage.setItem(
-      `file-Structure-assignment-${id}`,
-      JSON.stringify(fileStructure)
-    );
-  }, [fileStructure, id]);
-
-  useEffect(() => {
-    if (isSavingError) {
-      console.error("Error saving file:", savedFile);
+      console.error("Error submiting assignment", error);
       toast({
-        title: "Erro ao salvar o arquivo",
-        description: "Ocorreu um erro ao salvar o arquivo.",
+        title: "Ocorreu um erro ao submeter sua tarefa",
         variant: "destructive",
       });
-      return;
-    }
-    if (!isSaving && savedFile) {
+    },
+    onSuccess: (data) => {
+      console.log(data);
+      //TODO: handle some animations here
       toast({
-        title: "Arquivo salvo com sucesso",
+        title: "Seu trabalho foi enviado com sucesso",
         variant: "default",
       });
+    },
+  });
+
+  React.useEffect(() => {
+    if (activeFile.id && assignmentData?.id) {
+      const fileKey = `assignment-${assignmentData.id}-file-${activeFile.id}`
+      fetchFileContentFromStashAsync(fileKey)
+        .then((data) => {
+          if (data) {
+            setDefaultEditorValue(data.toString());
+          }
+        })
     }
-  }, [savedFile, isSaving, isSavingError]);
+  }, [activeFile.id, assignmentData?.id, fetchFileContentFromStashAsync]);
 
-  const handleEditorChange = (value: string | undefined) => {
-    if (value !== undefined) {
-      const updatedStructure = [...fileStructure];
-      // Find and update the file content by path
-      const updateFileContent = (items: FileType[]): boolean => {
-        for (const item of items) {
-          // Check if this is the active file by comparing paths
-          if (
-            item.type === "file" &&
-            getFilePath(item, fileStructure) === activeFile
-          ) {
-            item.lastModified = new Date();
-            return true;
-          }
+  const { mutate: saveFileInServer } = useMutation({
+    mutationKey: ["save-file-in-saver"],
+    mutationFn: async () => {
+      const fileKey = `assignment-${assignmentData?.id}-file-${activeFile.id}`;
+      const fileContent = await fetchFileContentFromStashAsync(fileKey);
 
-          // Recursively search children
-          if (item.children && updateFileContent(item.children)) {
-            return true;
-          }
-        }
-        return false;
-      };
+      const file = new File([fileContent], activeFile.name, {
+        type: "text/plain",
+      });
+      return FileSaverService.uploadFileToServer(file, Number(id));
+    },
+  });
 
-      updateFileContent(updatedStructure);
-      setFileStructure(updatedStructure);
-      setActiveFileContent(value);
+  React.useEffect(() => {
+    if (id) {
+      setTreeData(defaultTreeData);
+    }
+  }, [id]);
+
+  const handleFileSelect = (fileId: string) => {
+    const selectedFile = treeData
+      .flatMap((item) => item.children || [])
+      .find((file) => file.id === fileId);
+    if (selectedFile) {
+      setActiveFile({
+        id: selectedFile.id,
+        name: selectedFile.label,
+      });
+      const fileKey = `assignment-${assignmentData?.id}-file-${fileId}`;
+      fetchFileContentFromStash(fileKey);
     }
   };
 
-  useEffect(() => {
-    if (activeLocalFilePath && assignment && activeFileContent === "") {
-      const fileStruct = getFileStruct(activeLocalFilePath);
-      if (!fileStruct) {
-        console.error(
-          "File structure not found for path:",
-          activeLocalFilePath
-        );
-        return;
-      }
-      const fileKey = `${assignment.id}/${fileStruct.id}/${activeLocalFilePath}`;
-      getFileContentFromStash(fileKey);
-    }
-  }, [activeFileContent, activeLocalFilePath, assignment, getFileContentFromStash, getFileStruct]);
-
   const handleRun = () => {
-    if (activeFileContent === "") {
-      toast({
-        title: "Erro",
-        description: "O arquivo está vazio.",
-        variant: "destructive",
-      });
-      return;
-    }
-    setConsoleOutput(["Enviando para teste..."]);
     submitAssignment();
   };
 
-  const handleFileLocalSave = () => {
-    const struct = getFileStruct(activeLocalFilePath);
-    if (!struct) {
-      console.error("Error getting struct");
-      return;
-    }
-    saveFileInStash(struct);
+  const handleServerSave = () => {
+    saveFileInServer();
   };
 
-  const handleSave = () => {
-    if (activeFileContent === "") {
-      toast({
-        title: "Erro",
-        description: "O arquivo está vazio.",
-        variant: "destructive",
+  const handleEditorChange = (value: string | undefined) => {
+    if (value !== undefined) {
+      const fileKey = `assignment-${assignmentData?.id}-file-${activeFile.id}`;
+      const fileData: FileStrucutre = {
+        name: activeFile.name,
+        data: value,
+        size: value.length,
+        createdAt: Date.now().toString(),
+        updateAt: Date.now().toString(),
+      };
+      saveFileInStash({
+        fileData,
+        fileKey,
       });
-      return;
     }
-    saveFileAtServer(
-      new File([activeFileContent], activeLocalFilePath, {
-        type: "text/plain",
-      })
-    );
   };
 
-  const cleanActiveFileAndContent = () => {
-    setActiveFile("");
-    setActiveFileContent("");
-  };
-
-  if (isLoading || !assignment) {
+  if (isAssignmentLoading) {
     return (
       <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
-        <span className="ml-4 text-lg">Carregando...</span>
+        <p className="text-lg text-gray-500">Carregando atividade...</p>
       </div>
     );
   }
 
   return (
-    <>
-      <WorkspaceHeader assignmentDescription={assignment.description} />
+    <div className="h-screen text-foreground flex flex-col">
+      <WorkspaceHeader
+        assignment={{
+          title: assignmentData !== undefined ? assignmentData.title : "",
+          description:
+            assignmentData !== undefined ? assignmentData.description : "",
+        }}
+        onRunClick={handleRun}
+        onSaveClick={handleServerSave}
+      />
 
-      {focusCount === 5 && (
-        <WindowFocusDialog onClick={() => resetFocusCount()} />
-      )}
+      <div className="flex flex-1 min-h-0">
+        <WorkspaceExplorer
+          treeData={treeData}
+          onFileSelect={handleFileSelect}
+        />
 
-      <div className="flex flex-1 overflow-hidden">
-        <div className="flex-1 flex flex-col overflow-hidden">
-          <div className="flex flex-1 overflow-hidden explorer-panel">
-            <WorkspaceExplorer
-              explorerWidth={explorerWidth}
-              fileStructure={fileStructure}
-              startResize={startResize}
-              activeFile={activeFile}
-              newItem={newItem}
-              setNewItem={setNewItem}
-              cleanActiveFileAndContent={cleanActiveFileAndContent}
-              setFileStructure={setFileStructure}
-            />
-            <div className="flex-1 flex flex-col overflow-hidden">
-              <WorkspaceEditor
-                activeFile={activeFile}
-                activeFileContent={activeFileContent}
-                handleEditorChange={handleEditorChange}
-                handleRun={handleRun}
-                isPending={isPending}
-                handleSave={handleSave}
-                isSavingAtServer={isSavingAtServer}
-                handleLocalSave={handleFileLocalSave}
-              />
-              <WorkspaceConsole
-                consoleHeight={consoleHeight}
-                startResize={startResize}
-              />
-            </div>
-          </div>
+        <div className="flex-1 flex flex-col">
+          <WorkspaceCodeEditor
+            activeFile={activeFile.name}
+            editorValue={fileContent?.toString()}
+            editorDefaultValue={defaultEditorValue}
+            onEditorChange={handleEditorChange}
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 }
