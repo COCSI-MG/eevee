5c549a20955efbb30e69e417ee6c486817e7bf31
devgui <guilhermepereiradv@gmail.com>
2025-07-14 22:22:56 -0300

    fix(hooks): initialize focusCount to 1 and add event listener for preventing default actions

--- CEFETCodeLab-Web/src/hooks/use-window-focus.ts
+++ CEFETCodeLab-Web/src/hooks/use-window-focus.ts
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 const MAX_FOCUS_LIMIT = 8;
 
 const useWindowFocus = () => {
-  const [focusCount, setFocusCount] = useState(0);
+  const [focusCount, setFocusCount] = useState<number>(1);
   const { toast } = useToast();
   const { back } = useRouter();
 
@@ -15,6 +15,10 @@ const useWindowFocus = () => {
     });
   }, []);
 
+  const eventListenerPreventDefault = (e: MouseEvent | KeyboardEvent) => {
+    e.preventDefault();
+  }
+
   useEffect(() => {
     if (focusCount > MAX_FOCUS_LIMIT) {
       toast({
@@ -23,8 +27,14 @@ const useWindowFocus = () => {
         variant: "destructive",
       });
 
+      document.addEventListener('click', eventListenerPreventDefault);
+
+      document.addEventListener('keydown', eventListenerPreventDefault);
+
       setTimeout(() => {
         setFocusCount(0);
+        document.removeEventListener('keydown', eventListenerPreventDefault);
+        document.removeEventListener('click', eventListenerPreventDefault);
         back();
       }, 5000);
     }
