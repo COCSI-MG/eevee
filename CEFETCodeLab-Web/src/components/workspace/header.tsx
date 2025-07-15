08870d00d2e020c06d70c5738e43bb7c0eda023d
devgui <guilhermepereiradv@gmail.com>
2025-07-14 21:57:06 -0300

    feat(header): enhance workspace header with run and save buttons

--- CEFETCodeLab-Web/src/components/workspace/header.tsx
+++ CEFETCodeLab-Web/src/components/workspace/header.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { Code, Info } from "lucide-react";
+import { Code, Info, Play, Save } from "lucide-react";
 import { useRouter } from "next/navigation";
 import {
   Dialog,
@@ -11,13 +11,18 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
+import { Assignment } from "@/app/interface/scheduler-api/assignment";
 
 interface WorkspaceHeaderProps {
-  assignmentDescription?: string;
+  assignment: Pick<Assignment, "title" | "description">;
+  onRunClick: () => void;
+  onSaveClick: () => void;
 }
 
 const WorkspaceHeader: React.FC<WorkspaceHeaderProps> = ({
-  assignmentDescription,
+  assignment,
+  onRunClick,
+  onSaveClick,
 }) => {
   const { back } = useRouter();
 
@@ -34,22 +39,39 @@ const WorkspaceHeader: React.FC<WorkspaceHeaderProps> = ({
           <DialogContent>
             <DialogHeader>
               <DialogTitle className="text-lg font-semibold">
-                Descrição da Atividade
+                {assignment?.title}
               </DialogTitle>
             </DialogHeader>
             <div className="p-4 text-sm text-slate-300">
-              {assignmentDescription || "Nenhuma descrição fornecida."}
+              {assignment?.description || "Nenhuma descrição fornecida."}
             </div>
           </DialogContent>
         </Dialog>
       </div>
-      <div className="flex items-center gap-4">
+
+      <div className="flex items-center gap-2">
         <Button variant="ghost" size="sm" onClick={() => back()}>
           Assigments
         </Button>
         <Button variant="ghost" size="sm">
           Ajuda
         </Button>
+
+        <div className="flex items-center gap-2 ml-4">
+          <Button
+            size={"sm"}
+            className="bg-green-600 hover:bg-green-700 text-white"
+            onClick={onRunClick}
+          >
+            <Play className="w-4 h-4 mr-1" />
+            Run
+          </Button>
+
+          <Button size={"sm"} variant={"outline"} onClick={onSaveClick}>
+            <Save className="w-4 h-4 mr-1" />
+            Save
+          </Button>
+        </div>
       </div>
     </header>
   );
