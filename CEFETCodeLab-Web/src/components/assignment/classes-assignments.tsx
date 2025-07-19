c6d5ef55175c616eebe88952f89321e0be932bdd
devgui <guilhermepereiradv@gmail.com>
2025-07-19 12:08:36 -0300

    feat(assignments): implement ClassesAssignments component to display assignments for a class

---
+++ CEFETCodeLab-Web/src/components/assignment/classes-assignments.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { AssignmentService } from "@/app/integration/scheduler-api/assignment";
+import { useQuery } from "@tanstack/react-query";
+import { useParams, useRouter } from "next/navigation";
+import Loader from "../loader";
+import AssignmentsCard from "./assignments-card";
+import { Button } from "../ui/button";
+import { ArrowLeft } from "lucide-react";
+
+export default function ClassesAssignments() {
+  const { id } = useParams();
+  const { back } = useRouter();
+
+  const { data, isSuccess, isPending } = useQuery({
+    queryKey: ["assignments", id],
+    refetchOnWindowFocus: true,
+    initialData: [],
+    queryFn: () => AssignmentService.GetAssignmentsByClassId(Number(id)),
+  });
+
+  if (isPending) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      <div className="mb-8">
+        <Button variant="outline" className="mb-4" onClick={() => back()}>
+          <ArrowLeft className="mr-2" />
+          Voltar
+        </Button>
+        <h1 className="text-3xl font-bold tracking-tight mb-2">
+          Tarefas da Turma
+        </h1>
+        <p className="text-muted-foreground mt-1">
+          Aqui você pode ver todas as tarefas atribuídas a esta turma.
+        </p>
+      </div>
+
+      {isSuccess && data.length > 0 ? (
+        <AssignmentsCard data={data} />
+      ) : (
+        <p>Nenhuma tarefa encontrada para esta turma.</p>
+      )}
+    </>
+  );
+}
