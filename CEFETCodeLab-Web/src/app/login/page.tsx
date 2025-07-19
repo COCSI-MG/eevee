514a93e6cab023d31cb4dd94fba4f8c9e6501600
devgui <guilhermepereiradv@gmail.com>
2025-07-19 12:10:39 -0300

    fix(login): redirect to Classes route instead of Assignment after successful login

--- CEFETCodeLab-Web/src/app/login/page.tsx
+++ CEFETCodeLab-Web/src/app/login/page.tsx
@@ -64,7 +64,7 @@ export default function Login() {
         push('/admin');
         return;
       }
-      push(Route.Assignment);
+      push(Route.Classes);
     }
   }, [isError, error, data, isSuccess, push]);
 
