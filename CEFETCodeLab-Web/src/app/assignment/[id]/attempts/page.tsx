c6f16c3acbc3f46165978a99d28e46b13f36448c
devgui <guilhermepereiradv@gmail.com>
2025-07-27 16:29:18 -0300

    feat(attempts-page): create AssignmentAttemptsPage component to display attempts

---
+++ CEFETCodeLab-Web/src/app/assignment/[id]/attempts/page.tsx
@@ -0,0 +1,9 @@
+import AttemptsCard from "@/components/attempts/attempts-card";
+
+export default function AssignmentAttemptsPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <AttemptsCard />
+    </div>
+  );
+}
