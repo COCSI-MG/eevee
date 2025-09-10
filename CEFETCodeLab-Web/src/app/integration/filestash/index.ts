d10753edccd53e7d388936690feb517bef9e1185
Guilherme Pereira <guilhermepereiradv@gmail.com>
2025-09-09 21:20:59 -0300

    refactor(filestash): improve code formatting and consistency in async functions

--- CEFETCodeLab-Web/src/app/integration/filestash/index.ts
+++ CEFETCodeLab-Web/src/app/integration/filestash/index.ts
@@ -3,36 +3,35 @@ import { FileStash, PublicFileSchema } from "filestash";
 const DB_VERSION = 1 //increment when schema changes
 
 export async function initStash() {
-    const db = new FileStash<PublicFileSchema>("EEVEE-DB");
-    return await db.makeStores(
-        trans => trans.createStore("files")
-    ).version(DB_VERSION).open();
+  const db = new FileStash<PublicFileSchema>("EEVEE-DB");
+  return await db.makeStores(
+    trans => trans.createStore("files")
+  ).version(DB_VERSION).open();
 }
 
 export async function upsertFileInStash(data: PublicFileSchema['files']['Schema'], key: PublicFileSchema['files']['Key']) {
-    try {
-        const db = new FileStash<PublicFileSchema>("EEVEE-DB");
-        await db.version(DB_VERSION).open();
-        const { files } = db.transactionWrite(["files"]).stores;
-        const result = await files.upsert(data, key);
-        db.destructor();
-        return result;
-    } catch (error) {
-        console.error("Error upserting file in Filestash:", error);
-        throw error;
-    }
+  try {
+    const db = new FileStash<PublicFileSchema>("EEVEE-DB");
+    await db.version(DB_VERSION).open();
+    const { files } = db.transactionWrite(["files"]).stores;
+    const result = await files.upsert(data, key);
+    db.destructor();
+    return result;
+  } catch (error) {
+    console.error("Error upserting file in Filestash:", error);
+    throw error;
+  }
 }
 
 export async function getFileFromStash(fileKey: string) {
-    try {
-        const db = new FileStash<PublicFileSchema>("EEVEE-DB");
-        await db.version(DB_VERSION).open();
-        const files = db.transactionRead(["files"]).files;
-        const result = await files.get(fileKey);
-        db.destructor();
-        return result;
-    } catch (error) {
-        console.error("Error getting file from Filestash:", error);
-        throw error;
-    }
+  try {
+    const db = new FileStash<PublicFileSchema>("EEVEE-DB");
+    await db.version(DB_VERSION).open();
+    const files = db.transactionRead(["files"]).files;
+    const result = await files.get(fileKey);
+    db.destructor();
+    return result;
+  } catch (error) {
+    console.error("Error getting file from Filestash:", error);
+  }
 }
