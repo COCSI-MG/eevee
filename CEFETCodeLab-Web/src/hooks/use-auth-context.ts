135bf21154e8bd9890329e0eb6cfb2b246f7098b
Guilherme Pereira <guilhermepereiradv@gmail.com>
2025-09-30 23:55:27 -0300

    feat: add useAuthContext hook for accessing authentication context

--- CEFETCodeLab-Web/src/hooks/use-auth-context.ts
+++ CEFETCodeLab-Web/src/hooks/use-auth-context.ts
@@ -3,7 +3,7 @@
 import { AuthContext } from "@/app/provider/auth-provider"
 import { useContext } from "react";
 
-export const useAuthUser = () => {
+export const useAuthContext = () => {
   const context = useContext(AuthContext);
   if (!context) {
     throw new Error("useAuthUser must be used within an AuthProvider");
