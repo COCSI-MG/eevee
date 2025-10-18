e6d98ebe6106d449f4a9d603ecce6b74e418a57c
Guilherme Pereira <guilhermepereiradv@gmail.com>
2025-10-18 15:21:59 -0300

    fix: update useClasses hook to correct queryKey and ensure proper data fetching

--- CEFETCodeLab-Web/src/hooks/use-classes.ts
+++ CEFETCodeLab-Web/src/hooks/use-classes.ts
@@ -3,9 +3,11 @@ import { useQuery } from "@tanstack/react-query"
 
 const useClasses = () => {
     return useQuery({
-        queryKey: ["classes"],
-        initialData: [],
-        queryFn: () => ClassesService.listClasses(),
+        queryKey: ["admin-classes"],
+        queryFn: async () => {
+            return ClassesService.listClasses();
+        },
+        staleTime: 1000 * 60, // 1 minute
     })
 }
 
