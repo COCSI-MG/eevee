5d3ae8a25328b6bb70db6da2abc79a31605913a6
Guilherme Pereira <guilhermepereiradv@gmail.com>
2025-10-25 12:11:13 -0300

    fix(classes-page): refactor to use useClasses hook and implement handleDelete functionality

--- CEFETCodeLab-Web/src/app/admin/classes/page.tsx
+++ CEFETCodeLab-Web/src/app/admin/classes/page.tsx
@@ -1,9 +1,57 @@
+"use client";
+
 import Link from "next/link";
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { ClassesService } from "@/app/integration/scheduler-api/classes";
+import { toast } from "@/hooks/use-toast";
+import { AxiosError } from "axios";
+import { useClasses } from "@/hooks/use-classes";
+import Loader from "@/components/loader";
 import AdminClassesTable from "@/components/classes/admin-classes-table";
 
-export default async function ClassesPage() {
+export default function ClassesPage() {
+  const { data: classes, refetch, isFetching } = useClasses();
+
+  const handleDelete = async (id: number) => {
+    try {
+      await ClassesService.remove(id);
+      toast({
+        title: "Class deleted",
+        description: "The class has been successfully deleted.",
+        duration: 4000,
+      });
+      refetch();
+    } catch (err) {
+      console.error("Failed to delete class:", err);
+
+      if (err instanceof AxiosError && err.response) {
+        const apiMessage = err.response.data?.message;
+        if (apiMessage) {
+          toast({
+            title: "Error",
+            description: apiMessage,
+            variant: "destructive",
+            duration: 4000,
+          });
+          return;
+        }
+      }
+
+      toast({
+        title: "Error",
+        description:
+          err instanceof Error ? err.message : "Failed to delete class.",
+        variant: "destructive",
+        duration: 4000,
+      });
+    }
+  };
+
+  if (isFetching) {
+    return <Loader />
+  }
+
   return (
     <div className="space-y-6">
       <div className="flex items-center justify-between">
@@ -30,7 +78,7 @@ export default async function ClassesPage() {
       </div> */}
 
       <div className="border rounded-md">
-        <AdminClassesTable />
+        <AdminClassesTable classes={classes} handleDelete={handleDelete} />
       </div>
     </div>
   );
