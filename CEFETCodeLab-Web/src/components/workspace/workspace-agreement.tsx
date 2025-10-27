49e7f6ab7f5d78b9edd7531de8765b59c39f7456
Guilherme Pereira <guilhermepereiradv@gmail.com>
2025-10-27 19:53:02 -0300

    fix(#108): next js router was not mounted

--- CEFETCodeLab-Web/src/components/workspace/workspace-agreement.tsx
+++ CEFETCodeLab-Web/src/components/workspace/workspace-agreement.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { Checkbox } from "../ui/checkbox";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function WorkspaceAgreement({
   title,
