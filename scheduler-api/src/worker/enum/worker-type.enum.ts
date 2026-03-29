26db89a6fb28d6bc67def00e35f81bf468d46cba
Guilherme Pereira <guilhermepereiradv@gmail.com>
2026-03-29 11:26:38 -0300

    feat: add NODE_NESTJS_POSTGRESQL to WorkerType enum

--- scheduler-api/src/worker/enum/worker-type.enum.ts
+++ scheduler-api/src/worker/enum/worker-type.enum.ts
@@ -5,4 +5,5 @@ export enum WorkerType {
   NODE_GRPCJS = 'node_grpcjs',
   NODE_NEXTJS_CYPRESS = 'node_nextjs_cypress',
   NODE_DEFAULT_POSTGRESQL = 'node_default_postgresql',
+  NODE_NESTJS_POSTGRESQL = 'node_nestjs_postgresql',
 }
