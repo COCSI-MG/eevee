992ca133c8b88302b9261ee70daf9147f13c9f0f
Guilherme Pereira <guilhermepereiradv@gmail.com>
2026-03-30 22:43:11 -0300

    feat: add AdminAttempts route for managing attempts in admin panel

--- front/src/app/routes.ts
+++ front/src/app/routes.ts
@@ -8,6 +8,7 @@ export enum Route {
   AdminClasses = "admin/classes",
   AdminAssignmentCreate = "admin/assignments/create",
   AdminTemplate = "/admin/templates",
+  AdminAttempts = "/admin/attempts",
   AssignmenstUsersSuspensions = "suspensions",
   Classes = "classes",
   ClassesAssignments = "classes/assignments",
