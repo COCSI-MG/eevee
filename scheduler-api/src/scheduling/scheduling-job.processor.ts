422ff390a530b54284768207b34f91dd29cf66ff
Guilherme Pereira <guilhermepereiradv@gmail.com>
2026-04-02 15:05:50 -0300

    feat: add SchedulingJobProcessor for handling scheduling jobs in the queue

---
+++ scheduler-api/src/scheduling/scheduling-job.processor.ts
@@ -0,0 +1,21 @@
+import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Job } from 'bullmq';
+import { Logger } from '@nestjs/common';
+import { CreateSchedulingJobMessageDto } from './dto/create-scheduling-job-message.dto';
+import { SchedulingService } from './scheduling.service';
+
+@Processor('scheduling-queue')
+export class SchedulingJobProcessor extends WorkerHost {
+  private readonly logger = new Logger(SchedulingJobProcessor.name);
+
+  constructor(private readonly schedulingService: SchedulingService) {
+    super();
+  }
+
+  async process(job: Job<CreateSchedulingJobMessageDto>) {
+    this.logger.debug(
+      `Received scheduling job ${job.name} with data: ${JSON.stringify(job.data)}`,
+    );
+    await this.schedulingService.processJobAndWait(job.data);
+  }
+}
