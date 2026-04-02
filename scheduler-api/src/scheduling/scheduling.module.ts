af770e76f2f36a769c1707f29509afe9e2a3c93b
Guilherme Pereira <guilhermepereiradv@gmail.com>
2026-04-02 15:05:13 -0300

    feat: update SchedulingModule to use BullMQ and refactor providers

--- scheduler-api/src/scheduling/scheduling.module.ts
+++ scheduler-api/src/scheduling/scheduling.module.ts
@@ -4,12 +4,22 @@ import { SchedulingController } from './scheduling.controller';
 import { WorkerModule } from 'src/worker/worker.module';
 import { AttemptModule } from 'src/attempt/attempt.module';
 import { AssignmentModule } from 'src/assignment/assignment.module';
-import { SchedulerCreateJobPublisher } from './scheduler-create-job.publisher';
-import { KafkaModule } from 'src/kafka/kafka.module';
+import { BullMQModule } from 'src/bullmq/bullmq.module';
+import { ScorePolicyService } from './score-policy.service';
+import { WorkerPayloadBuilderService } from './worker-payload-builder.service';
+import { SchedulingAttemptTransitionService } from './scheduling-attempt-transition.service';
+import { SchedulingWorkerPreparationService } from './scheduling-worker-preparation.service';
 
 @Module({
+  imports: [WorkerModule, AttemptModule, AssignmentModule, BullMQModule],
   controllers: [SchedulingController],
-  providers: [SchedulingService, SchedulerCreateJobPublisher],
-  imports: [WorkerModule, AttemptModule, AssignmentModule, KafkaModule],
+  providers: [
+    SchedulingService,
+    ScorePolicyService,
+    WorkerPayloadBuilderService,
+    SchedulingWorkerPreparationService,
+    SchedulingAttemptTransitionService,
+  ],
+  exports: [SchedulingService],
 })
 export class SchedulingModule {}
