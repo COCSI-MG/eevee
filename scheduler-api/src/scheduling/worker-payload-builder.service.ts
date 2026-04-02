b2ddf864e6246ffae8bb613eef89e325d5513eee
Guilherme Pereira <guilhermepereiradv@gmail.com>
2026-04-02 15:05:35 -0300

    feat: add WorkerPayloadBuilderService for building worker payloads with template handling

---
+++ scheduler-api/src/scheduling/worker-payload-builder.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@nestjs/common';
+import { readFileAsString } from 'src/utils/template.utils';
+import { normalizeTemplateImportPaths } from 'src/utils/template-import-path.utils';
+import { CreateWorkerDto } from 'src/worker/dto/create-worker.dto';
+import { WorkerType } from 'src/worker/enum/worker-type.enum';
+import { WorkerService } from 'src/worker/worker.service';
+import { WorkerTestFile } from 'src/worker/worker.interfaces';
+
+@Injectable()
+export class WorkerPayloadBuilderService {
+  constructor(private readonly workerService: WorkerService) {}
+
+  async build(
+    workerType: WorkerType,
+    assignmentTemplates: any[],
+    baseWorkerData: CreateWorkerDto,
+    templateVariablesModuleContent?: string,
+  ): Promise<CreateWorkerDto> {
+    const strategy = this.workerService.getStrategy(workerType);
+    const { srcPath, testPath } = strategy.workerConfig;
+
+    const dependencies = [...(baseWorkerData.dependencies ?? [])];
+    const testFiles: WorkerTestFile[] = [];
+
+    const testFilesContent = await Promise.all(
+      assignmentTemplates.map(async (templateRelation) => {
+        const templateDependencies =
+          templateRelation.template.dependencies ?? [];
+        if (templateDependencies.length > 0)
+          dependencies.push(...templateDependencies);
+
+        const content = await readFileAsString(
+          templateRelation.template.filePath,
+        );
+
+        const normalizedContent = normalizeTemplateImportPaths({
+          content,
+          srcPath,
+          testPath,
+        });
+
+        testFiles.push({
+          templateId: templateRelation.template.id,
+          type: workerType,
+          content: normalizedContent,
+        });
+
+        return normalizedContent;
+      }),
+    );
+
+    return {
+      ...baseWorkerData,
+      testFilesContent,
+      testFiles,
+      dependencies,
+      templateVariablesModuleContent:
+        templateVariablesModuleContent ??
+        baseWorkerData.templateVariablesModuleContent,
+    };
+  }
+}
