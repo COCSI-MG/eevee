f70f1f7680c555f4fbe5335ea8ac946303cefdec
Guilherme Pereira <guilhermepereiradv@gmail.com>
2026-04-05 11:03:50 -0300

    fix: update next.config.ts to use environment variables for API URL and app environment

--- front/next.config.ts
+++ front/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_API_URL: 'http://localhost:3010/v1',
-    APP_ENV: 'development',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    APP_ENV: process.env.APP_ENV,
   },
   experimental: {
     optimizePackageImports: ['lucide-react'],
