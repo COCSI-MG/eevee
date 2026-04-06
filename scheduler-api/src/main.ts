74fd01caa7d2e957c6ac7ab7b3a6b23115a95759
Guilherme Pereira <guilhermepereiradv@gmail.com>
2026-04-05 22:22:24 -0300

    fix: update CORS configuration to allow requests from eeveecodelab.online

--- scheduler-api/src/main.ts
+++ scheduler-api/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       new RegExp(/localhost:\d+/),
-      new RegExp(/eeveecodelab\.(local|site|com)$/),
+      new RegExp(/eeveecodelab\.(local|site|com|online)$/),
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
     allowHeaders: 'Content-Type, Accept, Authorization',
