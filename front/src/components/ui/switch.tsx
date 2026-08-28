5bdc21e97e93264d257ad24b16cf834c994ffe55
Gabriel <gabriel.drbastos@gmail.com>
2026-08-28 17:35:41 -0300

    feat: permitir copiar e colar

--- front/src/components/ui/switch.tsx
+++ front/src/components/ui/switch.tsx
@@ -6,7 +6,7 @@ import * as SwitchPrimitive from "@radix-ui/react-switch"
 const Switch = React.forwardRef<
   React.ComponentRef<typeof SwitchPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SwitchPrimitive.Root>
->(({ className, ...props }, ref) => (
+>(({ ...props }, ref) => (
   <SwitchPrimitive.Root
     className={
       "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input"
